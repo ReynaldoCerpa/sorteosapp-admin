4bdd52f1226663ebd4fb7fe1c397a677307f7530
ReynaldoCerpa <reynaldocerpa07@gmail.com>
2021-12-05 14:05:24 -0800

    formated BoletoItem

--- src/components/BoletoItem.jsx
+++ src/components/BoletoItem.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components"
 
 const Container = styled.div`
@@ -18,20 +19,52 @@ const Title = styled.h4`
 const Value = styled.h4`
     margin: 0 1.2rem 0 0;
 `;
+const Block = styled.div`
+    display:flex;
+    flex-direction: column;
+`;
+const Row = styled.div`
+    display: flex;
+`;
 
 const BoletosItem = ({numBoleto, precio, fechaVenta, idColaborador, idCartera, idComprador, vendido}) => {
+    const [fecha, setFecha] = useState("N/A")
+    if(fechaVenta != null){
+        const arr = fechaVenta.split("T")
+        setFecha(arr[0])
+    }
     return (
         <Container>
-            <Title># Boleto:</Title>
-            <Value>{numBoleto}</Value>
-            <Title>Precio:</Title>
-            <Value>${precio} MXN</Value>
-            <Title>Fecha de venta:</Title>
-            <Value>{fechaVenta}</Value>
-            <Title>Fecha de entrega:</Title>
-            <Value>{idColaborador}</Value>
-            <Title>Entregada:</Title>
-            <Value>{idCartera}</Value>
+            <Block>
+                <Row>
+                    <Title># Boleto:</Title>
+                    <Value>{numBoleto}</Value>
+                </Row>
+                <Row>
+                    <Title>Precio:</Title>
+                    <Value>${precio} MXN</Value>
+                </Row>
+            </Block>
+            <Block>
+                <Row>
+                    <Title>Fecha de venta:</Title>
+                    <Value>{fecha}</Value>
+                </Row>
+                <Row>
+                    <Title>ID Colaborador:</Title>
+                    <Value>{idColaborador}</Value>
+                </Row>
+            </Block>
+            <Block>
+                <Row>
+                    <Title>ID Cartera:</Title>
+                    <Value>{idCartera}</Value>
+                </Row>
+                <Row>
+                    <Title>ID Comprador:</Title>
+                    <Value>{idComprador != null ? idComprador : "N/A"}</Value>
+                </Row>
+            </Block>
         </Container>
     )
 }
